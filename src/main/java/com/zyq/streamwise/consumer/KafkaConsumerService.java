2de0a865b85fc20ed76517dc31bdc3f9c25d65df
zyqghjlll <zyqghjlll@163.com>
2025-04-01 15:52:18 +0800

    deploy by docker; add monitor

--- src/main/java/com/zyq/streamwise/consumer/KafkaConsumerService.java
+++ src/main/java/com/zyq/streamwise/consumer/KafkaConsumerService.java
@@ -2,6 +2,7 @@ package com.zyq.streamwise.consumer;
 
 import com.zyq.streamwise.entity.RealTimeData;
 import com.zyq.streamwise.repository.RealTimeDataRepository;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,18 @@ public class KafkaConsumerService {
 
     private final RealTimeDataRepository repository;
 
-    public KafkaConsumerService(RealTimeDataRepository repository) {
+    private final StringRedisTemplate redisTemplate;
+
+    public KafkaConsumerService(RealTimeDataRepository repository, StringRedisTemplate redisTemplate) {
         this.repository = repository;
+        this.redisTemplate = redisTemplate;
     }
+
     @KafkaListener(topics = "real_time_data", groupId = "streamwise-group")
     public void consume(String message) {
         System.out.println("Received message: " + message);
 
+        redisTemplate.opsForValue().set("real_time_data", message);
         RealTimeData entity = new RealTimeData();
         entity.setContent(message);
         repository.save(entity);
